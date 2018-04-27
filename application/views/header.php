c0127c88140552ea8cdc932b15b1ab855dc1d798
dhenytakiya <38420034+dhenytakiya@users.noreply.github.com>
2018-04-27 23:51:29 +0700

    Initial commit

---
+++ application/views/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <link href="<?php echo base_url() ?>assets/gambar/logo5.png" rel="icon" type="image/png">
+    
+    <title>Sistem Perpustakaan</title>
+
+    <link href=" <?php base_url() ?> assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href=" <?php base_url() ?> assets/css/bootstrap.css" rel="stylesheet">
+
+    <link href=" <?php base_url() ?> assets/css/custom.css" rel="stylesheet">
+
+    <link href=" <?php base_url() ?> assets/js/bootstrap.js" rel="stylesheet">
+
+    <link href=" <?php base_url() ?> assets/js/bootstrap.min.js" rel="stylesheet">
+
+    
+  </head>
+
+  <body class="bodi">
---
+++ application/views/web.php
@@ -0,0 +1,55 @@
+<div class="container" >
+
+<!-- menu utama perpus -->
+
+	<div id="pageHeader">
+		<a href="#"><img id="logo" src="<?php base_url() ?> assets/gambar/logo1.png"></a>
+	<ul id="float">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Buku</a></li>
+    <li><a href="#">Anggota</a></li>
+    <li><a href="#">Transaksi</a></li>
+    <li><a href="#">Laporan</a></li>
+    <li><a href="#">User</a></li>
+    <li><a href="#">Logout</a></li>
+    <li><a href="javascript:void(0);" oneclick="myFunctiion()">=</a></li>
+</ul> 
+	</div>
+
+<!-- menu sorting buku -->
+
+
+	<div id="side1">
+		<div class="cari">
+			<select class="opt">
+				<option>Category Buku</option>
+				<option>Buku edisi 1</option>
+				<option>Buku edisi 2</option>
+				<option>Buku edisi 3</option>
+			</select>
+				<input type="search" name="search" placeholder="Cari Buku . . .">
+				<button>Search</button>
+		</div>
+
+		<br>
+		
+	</div>
+
+
+
+<!-- footer -->
+	<div id="pageFooter">
+		<center>
+			<div id="mdsos">
+				<a href="#"><img alt="Facebook" border="0" src="<?php base_url() ?> assets/gambar/fb.png" title="kirim nilai A" /></a>
+
+				<a href="#"><img alt="Instagram" border="0" src="<?php base_url() ?> assets/gambar/ig.png" title="Follow my IG" /></a>
+
+				<a href="#"><img alt="Youtube" border="0" src="<?php base_url() ?> assets/gambar/yt.png" title="Subscribe my Youtube" /></a>
+
+				<a href="#"><img alt="Google+" border="0" src="<?php base_url() ?> assets/gambar/gg.png" title="Add me on Google+" /></a>
+
+			</div>
+			</center>
+	</div>
+</div>
